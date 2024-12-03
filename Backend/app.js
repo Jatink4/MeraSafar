760811ca94aa7fa1f5854798838e4d7c10d8621f
Jatin Kumar <jatinchhauker@gmail.com>
2024-12-03 14:00:40 +0530

    captain auth also done

--- Backend/app.js
+++ Backend/app.js
@@ -2,17 +2,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectToDb = require('./src/db/db'); // Assuming your database connection is handled here
-const userRoute = require('./src/routes/user.route'); // Assuming user routes are defined here
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./src/db/db'); 
+const userRoute = require('./src/routes/user.route'); 
+const captainRoutes=require('./src/routes/captain.route')
 
 dotenv.config(); // Load environment variables
 connectToDb(); // Connect to the database
 
-const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -20,5 +23,6 @@ app.get('/', (req, res) => {
 
 // Define routes
 app.use('/users', userRoute); // Mount the user routes on /users
+app.use('/captains', captainRoutes);
 
 module.exports = app; // Export the app to be used in server.js
